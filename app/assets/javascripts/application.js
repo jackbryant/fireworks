18c79efc7c52464064a83ef436df281ebebfd4bb
Hamilton Chapman <hamchapman@gmail.com>
2013-12-11 16:43:32 +0000

    Got basic version working on edit show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require timeline
+//= require wavesurfer.min
+//= require main
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
