258bd522912c930c7ca6897f23b01c86cafd2aa7
Jack Bryant <jackbryant597@gmail.com>
2013-12-12 17:39:13 +0000

    identifies the correct song for each show

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,7 +2,7 @@ class TracksController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
 
   def create
-  	Show.last.update(track_url: params[:track_url])
+  	@show.update(track_url: params[:track_url])
   end
 
 end
