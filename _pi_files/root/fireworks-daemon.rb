5454221f0f8c43242d2f3d9c03515c6790324a61
Hamilton Chapman <hamchapman@gmail.com>
2013-12-18 15:56:08 +0000

    Re-added radio buttons and updated fireworks_daemon in pi_files

--- _pi_files/root/fireworks-daemon.rb
+++ _pi_files/root/fireworks-daemon.rb
@@ -3,14 +3,13 @@ require 'pi_piper'
 include PiPiper
 require 'pusher'
 require 'pusher-client'
-
+require 'macaddr'
 
 def new_led_pin
   led_pin = PiPiper::Pin.new(:pin => 9, :direction => :out)
   led_pin.off 
   led_pin
 end
-
 
 def download_timings url
   system "curl #{url} > /root/run.rb"
@@ -45,8 +44,6 @@ def on_failure
   led_pin.off  
 end 
 
-
-
 Pusher.app_id = '61655'
 Pusher.key = '26f4232b489d7c8a2e22'
 Pusher.secret = 'da6df554f835d0121657'
@@ -56,32 +53,29 @@ socket = PusherClient::Socket.new('26f4232b489d7c8a2e22', options)
 
 socket.connect(true)
 
-# would be good to detect the connection and blink the led.
-# but right now I don't care enough.
-
+Thread.new { socket.subscribe('presence-of-boards', :user_id => 10, :user_info => { :mac => "#{Mac.addr}" }) }
 
 # Channel name : test_channel
 # Event name : new_message
 # Event data : { "timing_url": "http://192.168.50.151:3000/shows/3/download", "mp3_url": "http://192.168.50.151:3000/assets/run.mp3" }
-
 
 socket['test_channel'].bind('new_message') do |data|
   data_as_hash = JSON.parse(data)
 
   timings_url = data_as_hash['timing_url']
   mp3_url = data_as_hash['mp3_url']
-
-  puts "timings_url : #{timings_url}"
-  puts "mp3_url : #{mp3_url}"
+  board_id = data_as_hash['board_id']
   
-  download_timings timings_url if timings_url
-  dowload_mp3 mp3_url if mp3_url
-  on_success 
+  if(Mac.addr == board_id)
+    download_timings timings_url if timings_url
+    dowload_mp3 mp3_url if mp3_url
+    on_success 
+  end
 end
 
 after :pin => 18, :goes => :high do
-    Process.spawn('ruby /root/run.rb',  [:out, :err]=>["log-fw", "w"]) 
-    on_success
+  Process.spawn('ruby /root/run.rb',  [:out, :err]=>["log-fw", "w"]) 
+  on_success
 end
 
 PiPiper.wait
