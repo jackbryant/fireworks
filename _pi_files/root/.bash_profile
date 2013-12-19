26448e8e0baee751d1ed8f9d13d69d0a8211af8f
dwkns <dwkns@me.com>
2013-12-19 17:46:36 +0000

    Updated the pi code

--- _pi_files/root/.bash_profile
+++ _pi_files/root/.bash_profile
@@ -4,3 +4,8 @@ alias fwstop='/etc/init.d/fireworks.sh stop'
 alias fwstart='/etc/init.d/fireworks.sh start'
 alias fwstatus='/etc/init.d/fireworks.sh status'
 alias fw='/etc/init.d/fireworks.sh restart'
+
+# Increase volume by 10%
+alias volu='sudo amixer set PCM -- $[$(amixer get PCM|grep -o [0-9]*%|sed 's/%//')+10]%'
+# Decrease volume by 10%
+alias vold='sudo amixer set PCM -- $[$(amixer get PCM|grep -o [0-9]*%|sed 's/%//')-10]%'
--- _pi_files/root/fireworks-daemon.rb
+++ _pi_files/root/fireworks-daemon.rb
@@ -3,14 +3,13 @@ require 'pi_piper'
 include PiPiper
 require 'pusher'
 require 'pusher-client'
-
+require 'macaddr'
 
 def new_led_pin
   led_pin = PiPiper::Pin.new(:pin => 9, :direction => :out)
   led_pin.off 
   led_pin
 end
-
 
 def download_timings url
   system "curl #{url} > /root/run.rb"
@@ -20,23 +19,6 @@ def dowload_mp3 url
    system "curl #{url} > /root/run.mp3"
 end
 
-def on_success 
-  led_pin = new_led_pin
-  puts "led_pin #{led_pin}"
-
-  led_pin.on
-  sleep 0.3
-  led_pin.off 
-  sleep 0.3
-  led_pin.on
-  sleep 0.3
-  led_pin.off 
-  sleep 0.3
-  led_pin.on
-  sleep 0.3
-  led_pin.off 
-end 
-
 def on_failure
   led_pin = new_led_pin
   puts "led_pin #{led_pin}" 
@@ -45,7 +27,28 @@ def on_failure
   led_pin.off  
 end 
 
+def flash_pin num
+  led_pin = new_led_pin
+  num.times do
+    led_pin.on
+    sleep 0.2
+    led_pin.off 
+    sleep 0.2
+  end
+end
 
+def count_down
+   flash_pin 3
+   sleep 0.5
+   flash_pin 2
+   sleep 0.5
+   flash_pin 1
+ end
+
+def fire_show
+  count_down
+  Process.spawn('ruby /root/run.rb',  [:out, :err]=>["log-fw", "w"]) 
+end
 
 Pusher.app_id = '61655'
 Pusher.key = '26f4232b489d7c8a2e22'
@@ -56,32 +59,38 @@ socket = PusherClient::Socket.new('26f4232b489d7c8a2e22', options)
 
 socket.connect(true)
 
-# would be good to detect the connection and blink the led.
-# but right now I don't care enough.
-
+Thread.new { socket.subscribe('presence-of-boards', :user_id => 10, :user_info => { :mac => "#{Mac.addr}" }) }
 
 # Channel name : test_channel
 # Event name : new_message
 # Event data : { "timing_url": "http://192.168.50.151:3000/shows/3/download", "mp3_url": "http://192.168.50.151:3000/assets/run.mp3" }
 
-
 socket['test_channel'].bind('new_message') do |data|
   data_as_hash = JSON.parse(data)
 
-  timings_url = data_as_hash['timing_url']
-  mp3_url = data_as_hash['mp3_url']
+  if  data_as_hash['fire'] == 'true'
+    fire_show
+  else
+    
+    timings_url = data_as_hash['timing_url']
+    mp3_url = data_as_hash['mp3_url']
+    board_id = data_as_hash['board_id']
+    # puts "Mac.addr #{Mac.addr}"
+    # puts "board_id #{board_id}"
+    if(Mac.addr == board_id)
+      
+      download_timings timings_url if timings_url
+      dowload_mp3 mp3_url if mp3_url
+      flash_pin 2
+    end
+  end
 
-  puts "timings_url : #{timings_url}"
-  puts "mp3_url : #{mp3_url}"
-  
-  download_timings timings_url if timings_url
-  dowload_mp3 mp3_url if mp3_url
-  on_success 
+
 end
 
 after :pin => 18, :goes => :high do
-    Process.spawn('ruby /root/run.rb',  [:out, :err]=>["log-fw", "w"]) 
-    on_success
+  count_down
+  Process.spawn('ruby /root/run.rb',  [:out, :err]=>["log-fw", "w"]) 
 end
 
 PiPiper.wait
